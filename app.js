29d742bd6a25bd11c28c152ea27396d4aab9c3c7
germanbelloni <gerbelloni@gmail.com>
2024-07-10 15:45:25 -0300

    changes and views

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
 const session = require("express-session");
 const { Mercaderia, Usuario } = require("./product.js");
 const connectDB = require("./database.js");
@@ -15,23 +17,50 @@ const secretKey = process.env.SECRET_KEY;
 connectDB();
 
 //Middleware
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(cookieParser());
 app.use(
   session({
     secret: secretKey,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
       secure: false,
+      maxAge: 60000,
+      httpOnly: true,
     },
   })
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const nuevoUsuario = new Usuario({ ...req.body });
+
+  try {
+    await nuevoUsuario.save();
+    res.status(201).json(nuevoUsuario);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error, no se pudo añadir el usuario", error });
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 //Login del usuario donde se genera el JWT
@@ -42,43 +71,40 @@ app.post("/login", async (req, res) => {
   const user = await Usuario.findOne({ username, password });
 
   if (!user) {
-    return res.status(401).send({ error: "Credenciales invalidas" });
+    return res.status(401).render("login");
   } else {
     const token = jwt.sign({ username }, secretKey, { expiresIn: "365d" });
     // return res.json({ token });
     res.cookie("token", token, { httpOnly: true, secure: false });
-
-    res.json({message: 'Inicio de sesion exitoso'})
+    //res.json({message: 'Inicio de sesion exitoso'})
+    res.render("index");
   }
 });
 
 //Midlleware para verificar el token JWT
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
+  const token =
+    req.cookies.token || req.headers["authorization"]?.split(" ")[1];
+  console.log(req.cookies.token);
   if (!token) return res.status(401).json({ error: "No token provided" });
 
   //Verificacion del token
   jwt.verify(token, secretKey, (err, decoded) => {
-    err
-      ? res.status(401).json({ error: "Invalid token" })
-      : (req.decoded = decoded);
+    err ? res.status(401).render("404") : (req.decoded = decoded);
     next();
   });
 };
 
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.render("index");
+});
+
 //1. Obtener todos los productos (Ruta protegida por token)
 app.get("/productos", verifyToken, async (req, res) => {
-  const { categoria } = req.query;
-  const query = !categoria
-    ? {}
-    : { categoria: { $regex: categoria, $options: "i" } };
   try {
-    const mercaderia = await Mercaderia.find(query);
-    mercaderia
-      ? res.json(mercaderia)
-      : res
-          .status(404)
-          .json({ message: "No se encontraron productos de esa categoria" });
+    const mercaderia = await Mercaderia.find();
+    mercaderia ? res.render("products", { mercaderia }) : res.render("404");
   } catch (error) {
     res.status(500).send("Error al buscar los productos");
   }
@@ -88,9 +114,9 @@ app.get("/productos", verifyToken, async (req, res) => {
 app.get("/productos/:id", verifyToken, async (req, res) => {
   const { id } = req.params;
 
-  const mercaderia = await Mercaderia.findById(id);
-  mercaderia
-    ? res.json(mercaderia)
+  const producto = await Mercaderia.findById(id);
+  producto
+    ? res.render('product', {producto})
     : res.status(404).json({ message: "Producto no encontrado" });
 });
 
@@ -128,11 +154,16 @@ app.get("/productos/nombre/:nombre", verifyToken, async (req, res) => {
 });
 
 //4. Agregar un producto
-app.post("/productos", verifyToken, async (req, res) => {
+
+app.get("/productos/add", verifyToken, async (req, res) => {
+  return res.render("addProduct");
+});
+
+app.post("/productos/", verifyToken, async (req, res) => {
   const nuevaMercaderia = new Mercaderia({ ...req.body, codigo: uuidv4() });
   try {
     await nuevaMercaderia.save();
-    res.status(201).json(nuevaMercaderia);
+    res.status(201).render("products", { mercaderia });
   } catch (error) {
     res
       .status(500)
@@ -141,6 +172,13 @@ app.post("/productos", verifyToken, async (req, res) => {
 });
 
 //5. Modificar el precio de un producto
+
+app.get("/productos/edit/:id", verifyToken, async (req, res) => {
+  const { id } = req.params;
+  const producto = Mercaderia.findById(id);
+  return res.render("editProduct", { producto });
+});
+
 app.patch("/productos/:id", verifyToken, async (req, res) => {
   const { id } = req.params;
   try {
@@ -168,12 +206,9 @@ app.patch("/productos/:id", verifyToken, async (req, res) => {
 app.delete("/productos/:id", verifyToken, async (req, res) => {
   const { id } = req.params;
   try {
-    const productoBorrado = await Mercaderia.findByIdAndDelete(id);
-    productoBorrado
-      ? res.json({ message: "Producto borrado con exito" })
-      : res
-          .status(404)
-          .json({ message: "No se encontro el producto para borrar" });
+    const mercaderia = await Mercaderia.findByIdAndDelete(id);
+    console.log(mercaderia);
+    mercaderia ? res.redirect('/productos') : res.status(404).render("404");
   } catch (error) {
     return res.status(500).json({ message: "Error al borrar el producto" });
   }
